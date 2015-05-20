fd24b5eb684a9c267c6a48d3bbb971d479b24048
sangnguyendinh <sang.nguyend.dev@gmail.com>
2015-05-20 09:31:17 +0700

    update

---
+++ Projects/Project/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by Nguyen Dinh Sang on 5/14/15.
+//  Copyright (c) 2015 Sang Nguyen Dinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
